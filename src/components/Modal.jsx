871df86e96ebe44bd2b83ba4f7f9df8742cc8387
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-01-17 01:55:45 -0300

    Actualizando

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,11 @@
 import CerrarBtn from "../img/cerrar.svg";
+import { useState } from "react";
 
 const Modal = ({ setModal, animarModal, setAnimarModal }) => {
+  const [nombre, setNombre] = useState("");
+  const [cantidad, setCantidad] = useState("");
+  const [categoria, setCategoria] = useState("");
+
   const ocultarModal = () => {
     setModal(false);
     setAnimarModal(false);
@@ -25,23 +30,31 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
             id='nombre'
             type='text'
             placeholder='Añade el Nombre del Gasto'
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
         </div>
 
-        <div className="campo">
+        <div className='campo'>
           <label htmlFor='cantidad'>Cantidad</label>
 
           <input
             id='cantidad'
             type='number'
             placeholder='Añade la Cantidad del gasto: ej. 300'
+            value={cantidad}
+            onChange={(e) => setCantidad(Number(e.target.value))}
           />
         </div>
 
-        <div className="campo">
+        <div className='campo'>
           <label htmlFor='categoria'>Categoria</label>
 
-          <select id='categoria'>
+          <select
+            id='categoria'
+            value={categoria}
+            onChange={(e) => setCategoria(e.target.value)}
+          >
             <option value=''>-- Seleccione</option>
             <option value='ahorro'>Ahorro</option>
             <option value='comida'>Comida</option>
