916d9de71f5eab604cca4d57eeee8a26e55a4141
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-01-18 16:46:10 -0300

    acutalizando

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ListadoGastos = () => {
+const ListadoGastos = ({gastos}) => {
   return (
     <div className='listado-gastos contenedor'>
-      <h2>Gastos</h2>
+      <h2>{gastos.length ? 'Gastos' : 'No Hay Gastos aún'}</h2>
 
     </div>
   )
